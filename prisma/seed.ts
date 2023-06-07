95b1366c6277f41fdb268d3af6275670ed4ac0ab
Gabriel Penteado <gabripenteado@gmail.com>
2023-06-07 00:41:24 -0300

    config seed.ts to sqlite in mobile

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,16 +3,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 const firstProcedureId = '0730ffac-d039-4194-9571-01aa2aa0efbd'
-const firstProcedureCreationDate = new Date('2023-01-01T00:00:00.000')
+const firstProcedureCreationDate = new Date('2023-01-01T03:00:00.000')
 
 const secondProcedureId = '00880d75-a933-4fef-94ab-e05744435297'
-const secondProcedureCreationDate = new Date('2023-01-01T00:00:00.000')
+const secondProcedureCreationDate = new Date('2023-01-01T03:00:00.000')
 
 const thirdProcedureId = 'fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00'
-const thirdProcedureCreationDate = new Date('2023-01-01T00:00:00.000')
+const thirdProcedureCreationDate = new Date('2023-01-01T03:00:00.000')
 
 const fourthProcedureId = '7376a39c-f050-11ed-a05b-0242ac120003'
-const fourthProcedureCreationDate = new Date('2023-01-01T00:00:00.000')
+const fourthProcedureCreationDate = new Date('2023-01-01T03:00:00.000')
 
 async function run() {
   await prisma.procedure.deleteMany()
@@ -108,7 +108,7 @@ async function run() {
     prisma.day.create({
       data: {
         /** Tuesday */
-        date: new Date('2023-01-01T00:00:00.000z'),
+        date: new Date('2023-01-01T03:00:00.000z'),
         // dayProcedures: {
         //   create: {
         //     procedure_id: firstProcedureId,
@@ -123,13 +123,13 @@ async function run() {
     // prisma.day.create({
     //   data: {
     /** Wednesday */
-    // date: new Date('2023-01-02T00:00:00.000z'),
-    // dayProcedures: {
-    //   create: [
-    //     { procedure_id: firstProcedureId },
-    //     { procedure_id: secondProcedureId },
-    //   ]
-    // }
+    //     date: new Date('2023-01-01T03:00:00.000z'),
+    //     dayProcedures: {
+    //       create: [
+    //         { procedure_id: firstProcedureId },
+    //         { procedure_id: secondProcedureId },
+    //       ]
+    //     }
     //   }
     // }),
 
@@ -139,14 +139,14 @@ async function run() {
     // prisma.day.create({
     //   data: {
     /** Thursday */
-    // date: new Date('2023-01-02T00:00:00.000z'),
-    // dayProcedures: {
-    //   create: [
-    //     { procedure_id: firstProcedureId },
-    //     { procedure_id: secondProcedureId },
-    //     { procedure_id: thirdProcedureId },
-    //   ]
-    // }
+    //     date: new Date('2023-01-01T03:00:00.000z'),
+    //     dayProcedures: {
+    //       create: [
+    //         { procedure_id: firstProcedureId },
+    //         { procedure_id: secondProcedureId },
+    //         { procedure_id: thirdProcedureId },
+    //       ]
+    //     }
     //   }
     // }),
 
@@ -156,15 +156,15 @@ async function run() {
     // prisma.day.create({
     //   data: {
     /** Thursday */
-    // date: new Date('2023-01-02T00:00:00.000z'),
-    // dayProcedures: {
-    //   create: [
-    //     { procedure_id: firstProcedureId },
-    //     { procedure_id: secondProcedureId },
-    //     { procedure_id: thirdProcedureId },
-    //     { procedure_id: fourthProcedureId },
-    //   ]
-    // }
+    //     date: new Date('2023-01-01T03:00:00.000z'),
+    //     dayProcedures: {
+    //       create: [
+    //         { procedure_id: firstProcedureId },
+    //         { procedure_id: secondProcedureId },
+    //         { procedure_id: thirdProcedureId },
+    //         { procedure_id: fourthProcedureId },
+    //       ]
+    //     }
     //   }
     // }),
 
