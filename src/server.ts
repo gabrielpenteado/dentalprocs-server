795da944904a42984493bb7c50b4a9a713dfdaf7
Gabriel Penteado <gabripenteado@gmail.com>
2024-09-21 17:03:35 -0300

    config to deploy in vercel

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { appRoutes } from './routes';
-
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
 
-app.register(cors); // Possible to config to only some address have access to the back-end 
+app.register(cors); // Possible to config to only some address have access to the back-end
 // "origin: 'http://endereço/rota'"
 
 app.register(appRoutes);
 
-app.listen({
-  port: process.env.PORT ? Number(process.env.PORT) : 3333,
-  host: '0.0.0.0',
-}).then((url) => {
-  console.log(`Server running on ${url}`);
-}).catch(err => console.log('Error:', err))
-
+app
+  .listen({
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+    host: "0.0.0.0",
+  })
+  .then((url) => {
+    console.log(`Server running on ${url}`);
+  })
+  .catch((err) => console.log("Error:", err));
